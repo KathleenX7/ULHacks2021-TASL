fba0844b1adfdfb3d89bb79d46743998b0b495f5
Vivian Dai <38384400+vivian-dai@users.noreply.github.com>
2021-08-14 01:13:41 -0400

    choose avatar done

--- registerscript.js
+++ registerscript.js
@@ -2,14 +2,54 @@
  * @fileoverview ULHacks register client code
  */
 
+const koroImages = [
+    "koro😐.png", 
+    "KoroAngry.png", 
+    "koroAngry2.png", 
+    "KoroCry.png", 
+    "koroFloor.png", 
+    "KoroGoo.png", 
+    "KoroHehe.png", 
+    "koroHuman.png",
+    "KoroNo.png",
+    "KoroOriginal.png",
+    "koroPink copy.png",
+    "koroSpike.png",
+    "koroWeirdSpike.png",
+    "koroWow.png",
+    "KoroYes.png"
+];
+
+let pfpCount = 0;
+
 var socket = io.connect();
 
 var nameText = document.getElementById("rName");
 var userText = document.getElementById("rUser");
 var passText = document.getElementById("rPass");
 
+function setImage() {
+    var pfp = document.getElementById("pfp");
+    pfp.src = `/Images/Koro/${koroImages[pfpCount%koroImages.length]}`;
+}
 
-function register() {
+function increasePfpCount() {
+    pfpCount++;
+    if(pfpCount > koroImages.length) {
+        pfpCount%=koroImages.length;
+    }
+    setImage();
+}
+
+function decreasePfpCount() {
+    pfpCount--;
+    while (pfpCount < 0) {
+        pfpCount += koroImages.length;
+    }
+    setImage();
+}
+
+function sendRegisterInfo() {
 }
 
 socket.on("login error", (errorMsg) => {
